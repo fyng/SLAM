bdce3e81fb8be9f74f7ceff01e83be928ae3c2fd
fyng <feiyangthegoat@gmail.com>
2024-04-04 09:24:28 -0400

    clean up comments and imports

--- SLAM/mapping.py
+++ SLAM/mapping.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from SLAM.utils.load_data import get_lidar, get_encoder, get_imu
-from SLAM.MapUtils.MapUtils import getMapCellsFromRay
+from SLAM.utils.MapUtils_fclad import getMapCellsFromRay_fclad
 
 class SLAM():
     def __init__(self, width=470, wheel_radius=127, enc_to_rev=360):
@@ -38,8 +38,6 @@ class SLAM():
         self.data.update({'acc_x': acc_x, 'acc_y': acc_y, 'acc_z': acc_z, 'gyro_x': gyro_x, 'gyro_y': gyro_y, 'gyro_z': gyro_z, 'ts_imu': ts})
     
     def dead_reckoning(self):
-        # FIXME! numbers look wrong
-        # splippage between (FL-RL), (FR-RR) < 0.1% across time course. Just take the front for now
         # [t, theta, x, y]
         n_timesteps = len(self.data['ts_encoder'])
 
@@ -58,6 +56,48 @@ class SLAM():
         self.pos = np.array(pos)
 
         return self.pos
+    
+    def map_lidar(self):
+        # get lidar data
+        lidar = self.data['lidar']
+        n_timesteps = len(lidar['t'])
+        map = np.zeros((n_timesteps, 2))
+        for i in range(n_timesteps):
+            ranges = lidar['scan'][i]
+            angles = lidar['angle']
+            indValid = np.logical_and((ranges < 30),(ranges> 0.1))
+            ranges = ranges[indValid]
+            angles = angles[indValid]
+
+            # init MAP
+            MAP = {}
+            MAP['res']   = 0.05 #meters
+            MAP['xmin']  = -20  #meters
+            MAP['ymin']  = -20
+            MAP['xmax']  =  20
+            MAP['ymax']  =  20
+            MAP['sizex']  = int(np.ceil((MAP['xmax'] - MAP['xmin']) / MAP['res'] + 1)) #cells
+            MAP['sizey']  = int(np.ceil((MAP['ymax'] - MAP['ymin']) / MAP['res'] + 1))
+
+            MAP['map'] = np.zeros((MAP['sizex'],MAP['sizey']),dtype=np.int8)#DATA TYPE: char or int8
+
+            # xy position in the sensor frame
+            xs0 = np.array([ranges*np.cos(angles)])
+            ys0 = np.array([ranges*np.sin(angles)])
+
+            # convert position in the map frame here
+            Y = np.concatenate([np.concatenate([xs0,ys0],axis=0),
+                np.zeros(xs0.shape)],axis=0)
+            ### HERE
+            # convert from meters to cells
+            xis = np.ceil((xs0 - MAP['xmin']) / MAP['res'] ).astype(np.int16)-1
+            yis = np.ceil((ys0 - MAP['ymin']) / MAP['res'] ).astype(np.int16)-1
+
+            r2 = getMapCellsFromRay_fclad(
+                np.ceil(-MAP['xmin']/ MAP['res']) - 1,
+                np.ceil(-MAP['ymin']/ MAP['res']) - 1,
+                xis[0],yis[0], 400)
+
 
 
     
