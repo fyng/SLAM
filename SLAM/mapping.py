95245c0f2c9807cf647ea7d6800b51231e4ab886
fyng <feiyangthegoat@gmail.com>
2024-04-04 13:20:01 -0400

    deal with numpy array dimensions better

--- SLAM/mapping.py
+++ SLAM/mapping.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from SLAM.utils.load_data import get_lidar, get_encoder, get_imu
-from SLAM.utils.MapUtils_fclad import getMapCellsFromRay_fclad
+from SLAM.MapUtilsCython import MapUtils_fclad as maputils
+from SLAM.MapUtils import MapUtils as maputils_py
 
 class SLAM():
     def __init__(self, width=470, wheel_radius=127, enc_to_rev=360):
@@ -12,11 +13,33 @@ class SLAM():
 
         self.data = {}
         self.pos = None 
-        self.map = None
+        self.map, self.map_meta = self._init_map()
+        # todo tune this
+        self.logodds_occ = 0.9
+        self.logodds_free = -0.4
+
+    def _init_map(self):
+        # init MAP
+        MAP = {}
+        MAP['res']   = 0.05 #meters
+        MAP['xmin']  = -30  #meters
+        MAP['ymin']  = -30 # seems good enough from the current plots
+        MAP['xmax']  =  30
+        MAP['ymax']  =  30
+        MAP['sizex']  = int(np.ceil((MAP['xmax'] - MAP['xmin']) / MAP['res'] + 1)) #cells
+        MAP['sizey']  = int(np.ceil((MAP['ymax'] - MAP['ymin']) / MAP['res'] + 1))
+        map = np.zeros((MAP['sizex'],MAP['sizey']),dtype=np.int8) #DATA TYPE: char or int8
+        return map, MAP
 
     def _encoder_ticks_to_dist(self, ticks):
         radians = ticks / self.enc_to_rev * 2 * np.pi 
         return radians * self.wheel_radius
+    
+    def _x_meters_to_cells(self, x):
+        return np.array([np.ceil((x - self.map_meta['xmin']) / self.map_meta['res']).astype(np.int16)-1])
+    
+    def _y_meters_to_cells(self, y):
+        return np.array([np.ceil((y - self.map_meta['ymin']) / self.map_meta['res']).astype(np.int16)-1])
 
     def load_encoder(self, path):
         # load encoder data and convert to distance
@@ -25,12 +48,12 @@ class SLAM():
         FR = self._encoder_ticks_to_dist(FR)
         RL = self._encoder_ticks_to_dist(RL)
         RR = self._encoder_ticks_to_dist(RR)
-        self.data.update({'FL': FL, 'FR': FR, 'RL': RL, 'RR': RR, 'ts_encoder': ts})
+        self.data.update({'FL': FL, 'FR': FR, 'RL': RL, 'RR': RR, 't_encoder': ts})
     
     def load_lidar(self, path):
         # dictionary of lidar data
         # {'t': timestamp, 'scan': range, 'angle': angle}
-        lidar = get_lidar(path)
+        lidar = get_lidar(path)        
         self.data.update({'lidar': lidar})
 
     def load_imu(self, path):
@@ -39,64 +62,91 @@ class SLAM():
     
     def dead_reckoning(self):
         # [t, theta, x, y]
-        n_timesteps = len(self.data['ts_encoder'])
+        n_timesteps = len(self.data['t_encoder'])
 
         left = (self.data['FL'] + self.data['RL']) / 2
         right = (self.data['FR'] + self.data['RR']) / 2
 
         pos = np.zeros((n_timesteps, 4))
         x, y, theta = 0, 0, 0
-        for i, t in enumerate(self.data['ts_encoder']):
+        for i, t in enumerate(self.data['t_encoder']):
             theta += (right[i] - left[i]) / self.width
             
             x += (right[i] + left[i]) / 2 * np.cos(theta)
             y += (right[i] + left[i]) / 2 * np.sin(theta)
 
             pos[i] += np.array([t, theta, x, y])
+        
+        pos[:,-2:] /= 1000 # convert to meters. Probably build a unit engine too
         self.pos = np.array(pos)
 
-        return self.pos
+        x = self._x_meters_to_cells(self.pos[:,-2])
+        y = self._y_meters_to_cells(self.pos[:,-1])
+
+        return x, y
     
+    def update_map_occupancy(self, passthrough, obstables):
+        '''
+        Update occupancy grid map of log probabilities. 
+
+        Params:
+            passthrough: [2, P] array of (x, y) indices of free cells
+            obstables: [2, P_obst] array of (x, y) indices of occupied cells
+        '''
+        for i in range(passthrough.shape[1]):
+            x, y = passthrough[:, i]
+            self.map[x, y] += self.logodds_free
+
+        for i in range(obstables.shape[1]):
+            x, y = obstables[:, i]
+            self.map[x, y] += self.logodds_occ
+
+
     def map_lidar(self):
+        '''
+        Map lidar data: rays of (range, angle) 
+        to grid occupancy (x, y) on the map
+        '''
         # get lidar data
         lidar = self.data['lidar']
-        n_timesteps = len(lidar['t'])
-        map = np.zeros((n_timesteps, 2))
+        n_timesteps = len(lidar)
+        t_encoder = self.pos[:,0]
         for i in range(n_timesteps):
-            ranges = lidar['scan'][i]
-            angles = lidar['angle']
+            # align lidar and encoder time
+            i_aligned = np.abs(t_encoder - lidar[i]['t']).argmin()  
+            t = t_encoder[i_aligned]
+            theta_platform = self.pos[i_aligned, 1]
+            x_platform = self.pos[i_aligned, -2]
+            y_platform = self.pos[i_aligned, -1]
+
+            ranges = lidar[i]['scan'].reshape(-1)
+            angles = lidar[i]['angle'].reshape(-1) # in radians
             indValid = np.logical_and((ranges < 30),(ranges> 0.1))
             ranges = ranges[indValid]
             angles = angles[indValid]
 
-            # init MAP
-            MAP = {}
-            MAP['res']   = 0.05 #meters
-            MAP['xmin']  = -20  #meters
-            MAP['ymin']  = -20
-            MAP['xmax']  =  20
-            MAP['ymax']  =  20
-            MAP['sizex']  = int(np.ceil((MAP['xmax'] - MAP['xmin']) / MAP['res'] + 1)) #cells
-            MAP['sizey']  = int(np.ceil((MAP['ymax'] - MAP['ymin']) / MAP['res'] + 1))
+            # xy position in platform frame
+            xs = ranges*np.cos(angles+theta_platform) + x_platform
+            ys = ranges*np.sin(angles+theta_platform) + y_platform
 
-            MAP['map'] = np.zeros((MAP['sizex'],MAP['sizey']),dtype=np.int8)#DATA TYPE: char or int8
-
-            # xy position in the sensor frame
-            xs0 = np.array([ranges*np.cos(angles)])
-            ys0 = np.array([ranges*np.sin(angles)])
-
-            # convert position in the map frame here
-            Y = np.concatenate([np.concatenate([xs0,ys0],axis=0),
-                np.zeros(xs0.shape)],axis=0)
-            ### HERE
             # convert from meters to cells
-            xis = np.ceil((xs0 - MAP['xmin']) / MAP['res'] ).astype(np.int16)-1
-            yis = np.ceil((ys0 - MAP['ymin']) / MAP['res'] ).astype(np.int16)-1
+            xis = self._x_meters_to_cells(xs)
+            yis = self._y_meters_to_cells(ys)
+            x = self._x_meters_to_cells(x_platform)[0]
+            y = self._y_meters_to_cells(y_platform)[0]
 
-            r2 = getMapCellsFromRay_fclad(
-                np.ceil(-MAP['xmin']/ MAP['res']) - 1,
-                np.ceil(-MAP['ymin']/ MAP['res']) - 1,
-                xis[0],yis[0], 400)
+            r2 = maputils.getMapCellsFromRay_fclad(
+                x, y,
+                xis.reshape(-1), yis.reshape(-1),
+                self.map_meta['sizex']
+            )
+
+            # update map occupancy
+            self.update_map_occupancy(passthrough=r2, obstables=np.concatenate((xis, yis), axis=0))
+
+        return self.map
+
+    
 
 
 
